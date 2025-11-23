7073b96c4469b4e0a21448d73783f90e0f38bf02
Trung Lê Hoàng <trunglh@pmr.vn>
2025-11-23 19:21:48 +0700

    klk

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace TestDocker.Controllers
             _logger = logger;
         }
 
-        [HttpGet("get")]
+        [HttpGet("get123")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
